480b8e3d29b621cef6e0bc7dc638f32c6f04a1f6
jveselkov <jveselkov@bk.ru>
2020-06-24 15:42:10 +0500

    settings edit

--- src/settings/index.js
+++ src/settings/index.js
@@ -1,6 +1,6 @@
 export const maxCells = 9
 export const maxRows = 9
 export const countColors = 5
-export const initialBallsCount = 65
+export const initialBallsCount = 5
 export const newBallsCount = 3
 export const countBallToErase = 5
